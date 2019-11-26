ed4731bea51c7edc1fac9875b5533a082aba78cc
DzungHoang <DzungHoang@gitlab.com>
2019-11-26 22:08:07 +0700

    mpi_bcast must be called by all machines, not just root machine

--- main.c
+++ main.c
@@ -11,12 +11,12 @@ void shuffle(int *array, size_t n);
     
 int main(int argc, char *argv[])
 {
-    int max_step = 100;
+    int max_step = 2;
     int batch_size = 64;
     int step = 0;
     float lr = 0.001;
     
-    int n_sample;
+    int n_samples;
     int data_dim;
 
     int machine_id;
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
     int ierr;
     double wtime;
 
+    int dummpy = 0;
     // Read Hyperparams
     // if (argc > 1) {
     //     max_step = atoi(argv[1]);
@@ -31,26 +32,27 @@ int main(int argc, char *argv[])
     
     // Read matrix data 
 
-    double **X = malloc(MAX_SIZE * sizeof(double *));
+    double **X = (double **) malloc(MAX_SIZE * sizeof(double *));
     for (int i = 0; i < MAX_SIZE; ++i)
         X[i] = malloc(MAX_SIZE * sizeof(double));
 
-    double *Y = malloc(MAX_SIZE * sizeof(double));
+    double *Y = (double *) malloc(MAX_SIZE * sizeof(double));
 
     FILE *file;
     file = fopen("matrix", "r");
     
-    fscanf(file, "%d", &n_sample);
+    fscanf(file, "%d", &n_samples);
     fscanf(file, "%d", &data_dim);
     
     data_dim = data_dim -1;
-    double *W = malloc(data_dim * sizeof(double));
-    
-    int *index = malloc(n_sample*sizeof(int));
-    for (int i=0;i<n_sample;i++) 
-        index[i] = i;
+    double *W = (double *) malloc(data_dim * sizeof(double));
+    double *grad = (double *) malloc(data_dim * sizeof(double));
+    double *part_grad = (double *) malloc(data_dim * sizeof(double));
 
-    for (int i = 0; i < n_sample; i++) {
+    int *index = (int *) malloc(n_samples*sizeof(int));
+
+    
+    for (int i = 0; i < n_samples; i++) {
         for (int j = 0; j < data_dim; j++)
             if (!fscanf(file, "%lf", &X[i][j]))
                 break;
@@ -60,6 +62,7 @@ int main(int argc, char *argv[])
 
     fclose(file);
     
+    int n_batches = (int) n_samples/batch_size;
     /*
         Initialize MPI.
     */
@@ -75,11 +78,16 @@ int main(int argc, char *argv[])
     */
     ierr = MPI_Comm_rank(MPI_COMM_WORLD, &machine_id);
 
+    if (machine_id==0) {
+        for (int i=0;i<n_samples;i++) 
+            index[i] = i;
+    }
+
     if (machine_id == 0)
     {   
         timestamp ( );
         printf("X data\n");
-        for (int i=0;i<n_sample;i++) {
+        for (int i=0;i<n_samples;i++) {
             for (int j=0;j<data_dim;j++) {
                 printf("%lf ",X[i][j]);
             }
@@ -88,49 +96,64 @@ int main(int argc, char *argv[])
         printf("\n");
 
         printf("Y data\n");
-        for (int i=0;i<n_sample;i++) {
+        for (int i=0;i<n_samples;i++) {
             printf("%lf ",Y[i]);
         }
         printf("\n");
         
         printf("The number of processes is %d\n", n_machines);
         printf("Number of steps: %d\n", max_step);
+
+        // Weight init
         for (int i=0;i<data_dim;i++) {
             W[i] = (double)rand()/(double)(RAND_MAX);
         }
+        for(int i=0;i<data_dim;i++) 
+            printf("Init W %lf\n", W[i]);
     }
 
-    while (step <= max_step)
+    // BCast init weight to all machine
+    ierr = MPI_Bcast (W, data_dim, MPI_DOUBLE, 0, MPI_COMM_WORLD );
+  
+    while (step < max_step)
     {
         if (machine_id == 0)
         {
             wtime = MPI_Wtime();
-            shuffle(index, n_sample);
-            for(int i =0;i<n_sample;i++)
-                printf("%d ",index[i]);
+            shuffle(index, n_samples);
         }
-        // suffle data
 
-
+        // BCast shuffled index to all machine
+        ierr = MPI_Bcast (index, n_samples, MPI_INT, 0, MPI_COMM_WORLD );
+        
         // split data 
         // ierr = MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
         // cal grad
         // primes_part = prime_number(n, id, p);
 
-        //combine grad
-        // ierr = MPI_Reduce(&primes_part, &primes, 1, MPI_INT, MPI_SUM, 0,
-        //                   MPI_COMM_WORLD);
+
+        // for (int i =0;i<data_dim;i++) {
+        //     grad[i] = W[i];
+        // }
+
+        // //combine grad
+        // ierr = MPI_Reduce(&part_grad, &grad, data_dim, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
         if (machine_id == 0)
         {
+            // for (int i =0;i<data_dim;i++) {
+            //     W[i] -= lr * grad[i];
+            // }
+            // ierr = MPI_Bcast (W, data_dim, MPI_DOUBLE, 0, MPI_COMM_WORLD );
             wtime = MPI_Wtime() - wtime;
             printf ( "Step %d time %14f\n", step, wtime );
         }
         // n = n * n_factor;
         step++;
     }
-
+    for(int i=0;i<data_dim;i++) 
+        printf("Machine %d: W %lf\n", machine_id, W[i]);
     /*
         Terminate MPI.
     */
